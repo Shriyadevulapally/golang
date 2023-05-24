c65201b9e2a0c7323659d2359add471b8b681afb
shriya santoshi <shriyadevulapally@gmail.com>
2023-05-23 19:35:20 -0500

    Float datatype program

---
+++ Float.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+var val1 float32 = 75.047716151
+var val2 float64 = 75.047896151
+
+func main() {
+	fmt.Println("Value is:", val1)
+	fmt.Println("Value is:", val2)
+
+}
